0023d72a79da0fca8edfd63bae289cfcc1c2057b
theowiik <theo.le.wiik@gmail.com>
2019-10-16 20:06:40 +0200

    Create Button.vue

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  router-link( :to="{ path }") {{text}}
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'Button',
+  props: {
+    text: String,
+    path: String,
+  },
+});
+</script>
