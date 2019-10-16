2486bffc11286f9481e4e14082452340bca86e8b
theowiik <theo.le.wiik@gmail.com>
2019-10-16 20:53:57 +0200

    Implement basic level 2

---
+++ frontend/src/views/Level2.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div
+    h1 Sad to see you go!
+    p You have now been succesfully unsubscribed! *
+
+    - for (var i = 0; i < 15; i++)
+      br
+
+    p(style="font-size: 7pt")
+      Button(path="/", text="* If you click this link")
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Button from '@/components/Button.vue';
+
+export default Vue.extend({
+  name: 'Level2',
+  components: {
+    Button,
+  },
+});
+</script>
