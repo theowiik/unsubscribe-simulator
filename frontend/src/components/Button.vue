1aa8ab60fe666579b373677b25aab79c498b809c
theowiik <theo.le.wiik@gmail.com>
2019-10-17 21:46:14 +0200

    add basic styling to button

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-  router-link( :to="{ path }") {{text}}
+  router-link( :to="{ path }")
+    button(class="bg-white hover:bg-gray-100 text-gray-800 py-1 px-2 border border-gray-400 rounded shadow") {{text}}
 </template>
 
 <script lang="ts">
