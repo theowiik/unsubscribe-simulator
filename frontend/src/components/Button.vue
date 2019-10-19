dcd80ef60eafb47c117db8f29939d3f302449cbc
theowiik <theo.le.wiik@gmail.com>
2019-10-19 20:36:44 +0200

    add success, danger and warning versions of button

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
   router-link( :to="{ path }")
-    button(class="bg-white hover:bg-gray-100 text-gray-800 py-1 px-2 border border-gray-400 rounded shadow") {{text}}
+    button(:class="[ \
+      'bg-white hover:bg-gray-100 py-1 px-2 border border-gray-400 rounded shadow', \
+      { \
+        'bg-yellow-500 hover:bg-yellow-800 text-white hover:text-gray-100': warning, \
+        'bg-green-500 hover:bg-green-800 text-white': success, \
+        'bg-red-600 hover:bg-red-800 text-white': danger \
+      } \
+      ]"
+    ) {{text}}
 </template>
 
 <script lang="ts">
@@ -10,6 +18,9 @@ export default Vue.extend({
   props: {
     text: String,
     path: String,
+    warning: Boolean,
+    success: Boolean,
+    danger: Boolean,
   },
 });
 </script>
