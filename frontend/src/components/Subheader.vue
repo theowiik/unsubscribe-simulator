a3dac99b9494b7cd8f83a730f9094140179d6c85
theowiik <theo.le.wiik@gmail.com>
2019-10-20 20:57:28 +0200

    Create Subheader.vue

---
+++ frontend/src/components/Subheader.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  h1(class="text-xl font-light text-gray-700 leading-tight") {{text}}
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'Subheader',
+  props: {
+    text: String,
+  },
+});
+</script>
