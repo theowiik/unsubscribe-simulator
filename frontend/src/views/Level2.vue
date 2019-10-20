cf3b72b3f2cf94701caa980ceb692e51a085cc8e
theowiik <theo.le.wiik@gmail.com>
2019-10-20 20:57:53 +0200

    improve styling on level 2

--- frontend/src/views/Level2.vue
+++ frontend/src/views/Level2.vue
@@ -1,13 +1,15 @@
 <template lang="pug">
-  div.text-center
-    Header(text="Sad to see you go 😥")
-    p.text-gray-600 You have now been unsuccesfully unsubscribed
+  div
+    .text-center
+      Header(text="Sad to see you go 😥")
+      p.text-gray-600 You have now been unsuccesfully unsubscribed
 
-    - for (var i = 0; i < 15; i++)
-      br
+      - for (var i = 0; i < 15; i++)
+        br
 
-    p(style="font-size: 7pt")
-      router-link( to="/3" class="text-gray-600 hover:text-gray-800" ) I still want to unsubscribe
+    .text-right
+      p(style="font-size: 7pt")
+        router-link( to="/3" class="text-gray-400 hover:text-gray-800" ) I still want to unsubscribe
 </template>
 
 <script lang="ts">
