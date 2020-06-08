9e928b5230faf30c35a3d80673a7765353e926f8
theowiik <42991003+theowiik@users.noreply.github.com>
2020-06-08 14:57:36 +0200

    Validate first 5 prime numbers

--- src/views/Level3.vue
+++ src/views/Level3.vue
@@ -2,22 +2,24 @@
   .text-center
     Header(text='Everyone knows that computers dont know what prime numbers are')
     Subheader(text='Please enter the first 5 prime numbers (separated by commas)')
-    
+
     .mt-10.flex.justify-center
       div(class='w-full sm:w-4/5 lg:w-2/5')
         input(
           class='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full \
                 py-2 px-4 text-gray-700 leading-tight focus:outline-none \
                 focus:bg-gray-100 focus:border-teal-200'
-          id='inline-full-name'
           type='text'
           placeholder='something, something else, ...'
+          v-on:input="checkIfValid"
         )
 
         .flex.mt-3.w-full.justify-end
           Button(text='Verify' path='/4' success)
 
     #loading.mt-10
+
+    p Valid: {{valid}}
 </template>
 
 <style>
@@ -57,6 +59,27 @@ export default Vue.extend({
     Button,
     Header,
     Subheader,
+  },
+  data() {
+    return {
+      valid: false,
+    };
+  },
+  methods: {
+    /**
+     * Checks if the input is a list of the first 5 prime numbers.
+     */
+    checkIfValid(event: InputEvent): void {
+      const regex = /^\s*2\s*,\s*3\s*,\s*5\s*,\s*7\s*,\s*1\s*1\s*$/;
+      const element = event.currentTarget as HTMLInputElement;
+      const currentText = element.value;
+
+      if (currentText === null) {
+        this.valid = false;
+      } else {
+        this.valid = regex.test(currentText);
+      }
+    },
   },
 });
 </script>
