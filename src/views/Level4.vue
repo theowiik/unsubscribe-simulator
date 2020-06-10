e98ef10fa8947456e0cd62fdcf271e7c6a34f642
theowiik <42991003+theowiik@users.noreply.github.com>
2020-06-10 22:53:11 +0200

    Add a delay between hovering and switching the unsub buttons

--- src/views/Level4.vue
+++ src/views/Level4.vue
@@ -1,25 +1,25 @@
-<template lang='pug'>
+<template lang="pug">
   .flex.justify-around
     div(v-if='leftPos')
-      div(@mouseover='leftPos = !leftPos')
+      div(@mouseover='onHover()')
         Button#button1(text='Unsubscribe' path='/5' light)
     div(v-else)
       Button#button2(text='Stay Subscribed' path='/' success)
 
     div(v-if='!leftPos')
-      div(@mouseover='leftPos = !leftPos')
+      div(@mouseover='onHover()')
         Button#button1(text='Unsubscribe' path='/5' light)
     div(v-else)
       Button#button2(text='Stay Subscribed' path='/' success)
 </template>
 
-<style scoped lang='sass'>
+<style scoped lang="sass">
 #unsubscribe-button:hover
   display: none
   transition: 1s ease
 </style>
 
-<script lang='ts'>
+<script lang="ts">
 import Vue from 'vue';
 import Button from '@/components/Button.vue';
 import Header from '@/components/Header.vue';
@@ -34,6 +34,15 @@ export default Vue.extend({
     return {
       leftPos: true,
     };
+  },
+  methods: {
+    async onHover() {
+      await this.sleep(100);
+      this.leftPos = !this.leftPos;
+    },
+    sleep(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
   },
 });
 </script>
