139f260a5a6d800bd67aa0844aa4959cdb4439f9
theowiik <42991003+theowiik@users.noreply.github.com>
2020-06-10 23:36:21 +0200

    Add FooterBanner

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<template lang='pug'>
+<template lang="pug">
   #app
     .flex.justify-center(class='mt-5 sm:mt-10')
       .container.px-4
         transition(name='fade' mode='out-in')
           router-view
+    FooterBanner
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import FooterBanner from '@/components/FooterBanner.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    FooterBanner,
+  },
+});
+</script>
---
+++ src/components/FooterBanner.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  footer.w-full.text-center.p-4.pin-b
+    p This site is under development.
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'FooterBanner',
+});
+</script>
